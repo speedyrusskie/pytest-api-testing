99b2602408b23faae71651b8cdc8444c78090ac3
speedyrusskie <daniel.chiriaev@gmail.com>
2022-02-09 19:36:10 -0800

    test files added

---
+++ tests/rick_and_morty_api/test_rickandmorty_validate_response.py
@@ -0,0 +1,7 @@
+import requests
+import pytest
+
+@pytest.mark.rickandmorty
+def test_validate_get_response_code():
+    response = requests.get("https://rickandmortyapi.com/api/character")
+    assert response.status_code == 200
